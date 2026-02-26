625cc03b6c111b075c4c2eb8ed1dbac33570b9ba
punpun41 <gdgddvvd@gmail.com>
2026-02-26 19:29:46 +0700

    test merge

--- app/src/main/java/com/example/mbg/MainActivity.kt
+++ app/src/main/java/com/example/mbg/MainActivity.kt
@@ -3,24 +3,70 @@ package com.example.mbg
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.example.mbg.onboarding.presentation.OnboardingScreen
-import com.example.mbg.ui.theme.MBGTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-class MainActivity : ComponentActivity() {
+import com.example.mbg.presentation.AnimatedSplashScreen
+import com.example.mbg.presentation.WelcomeScreen
+import com.example.mbg.ui.theme.MBGTheme
 
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         setContent {
             MBGTheme {
-                OnboardingScreen(
-                    onFinish = {}
+                var currentScreen by remember { mutableStateOf("splash") }
+
+                androidx.compose.animation.Crossfade(
+                    targetState = currentScreen,
+                    animationSpec = androidx.compose.animation.core.tween(durationMillis = 800),
+                    label = "transisi_layar"
                 )
+                { screen ->
+                    when (screen) {
+
+                        "splash" -> {
+                            AnimatedSplashScreen(
+                                onNavigateToWelcome = {
+                                    currentScreen = "welcome"
+                                }
+                            )
+                        }
+
+                        "welcome" -> {
+                            WelcomeScreen(
+                                onNavigateToLogin = {
+                                    currentScreen = "login"
+                                },
+                                onNavigateToRegister = {
+                                    currentScreen = "register"
+                                }
+                            )
+                        }
+
+                        // test
+                        "login" -> {
+                            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                                Text("Halaman masuk", fontSize = 24.sp)
+                            }
+                        }
+
+                        "register" -> {
+                            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                                Text("Halaman daftar", fontSize = 24.sp)
+                            }
+                        }
+
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/mbg/ui/theme/Type.kt
+++ app/src/main/java/com/example/mbg/ui/theme/Type.kt
@@ -7,34 +7,40 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.mbg.R
-
-val Poppins = FontFamily(
-    Font(R.font.poppins_regular, FontWeight.Normal),
-    Font(R.font.poppins_medium, FontWeight.Medium),
-    Font(R.font.poppins_bold, FontWeight.Bold),
-)
-
+// Set of Material typography styles to start with
 val Typography = Typography(
-
-    titleLarge = TextStyle(
-        fontFamily = Poppins,
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-        lineHeight = 28.sp
-    ),
-
-    bodyMedium = TextStyle(
-        fontFamily = Poppins,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        lineHeight = 20.sp
-    ),
-
     bodyLarge = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+
+)
+val poppins = FontFamily(
+    Font(resId = R.font.poppins_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.poppins_semibold, weight = FontWeight.SemiBold)
+)
+
+
+
+val inter = FontFamily(
+    Font(R.font.inter_regular)
 )
